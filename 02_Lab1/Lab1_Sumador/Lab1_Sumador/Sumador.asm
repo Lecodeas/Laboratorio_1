60896b944ab810690c8be2f704bd6ba58772438f
Lecodeas <edu.ianleu@gmail.com>
2024-01-31 19:31:14 -0600

    Implementación de Sumador
    INFO:
    - R16 es entradas de PINB
    - R17 es el Delay
    - R18 es estados anteriores
    - R19 es Contador 1
    - R20 es Contador 2
    - R21 es la combinacion de contadores
    - R22 es el registro de la suma
    
    UPDATES:
    - Cambio de entradas a PORTB y salidas a PORTD (contadores) y PORTC (suma)
    - Ahora los contadores siempre serán de 4 bits.
    - Implementado un método de desplegar ambos contadores en PORTD
    - Añadida La suma de 4 bits con carry externo.
    
    BUGS:
    - Al actualizar cualquier botón en PORTB, se pueden llegar a ejecutar varias instrucciones dependiendo de si está habilitado otro botón al mismo tiempo (Ejemplo: Mantengo presionado PB0 al momento de presionar PB1 y se ejecutan las operaciones de los botones presionados).
    
    NOTAS:
    - Funciona correctamente en el simulador, queda probarlo en el Nano.

--- 02_Lab1/Lab1_Sumador/Lab1_Sumador/Sumador.asm
+++ 02_Lab1/Lab1_Sumador/Lab1_Sumador/Sumador.asm
@@ -30,44 +30,48 @@ Setup:
 	LDI R16, 0b1000_0000 
 	STS CLKPR, R16 ; Habilitar prescaler (STS por la memoria en donde esta CLKPR)
 	LDI R16, 0b0000_0100
-	STS CLKPR, R16  ; 0011 es Divisor entre 16
+	STS CLKPR, R16  ; 0100 es Divisor entre 16
 
-	; Salidas (PORTB y PORTC a LEDs)
-	LDI R16, 0x0F
-	OUT DDRB, R16 ; Configura Primeros 4 de PORTB a Salida
-	OUT DDRC, R16 ; Configura Primeros 4 de PORTC a Salida
-	
-	; Entradas (PORTD a Buttons) 
-	CLR R17
+	; Salidas (PORTD y PORTC a LEDs)
 	LDI R16, 0xFF
-	OUT DDRD, R17 ; Configura todos de PORTD a Entradas
-	OUT PORTD, R16 ; Configurar todos Pull-Up
+	OUT DDRD, R16 ; Configura PORTD a Salida
+	OUT DDRC, R16 ; Configura PORTC a Salida
+	
+	; Entradas (PORTB a Buttons)
+	OUT DDRB, R17 ; Configura todos de PORTD a Entradas
+	OUT PORTB, R16 ; Configurar todos Pull-Up
 
 	; Registro para monitoreo de estados previos
 	LDI R18, 0xFF
 	
 	; Registros para Contadores
-	LDI R19, 0x00 ; Contador B
-	LDI R20, 0x00 ; Contador C
+	LDI R19, 0x00 ; Contador 1
+	LDI R20, 0x00 ; Contador 2
+	LDI R21, 0x00 ; OR de 1 y 2
+	LDI R22, 0x00 ; Suma de 1 y 2
 	
 
 ;-----------------------------------------------
 ; LOOP de flash memory
 
 Loop:
-	; Antirrebote de PinD
-	IN R16, PIND
+	; Antirrebote de PinB
+	IN R16, PINB
 	; Ya tengo estados previos en R19
 	CP R16, R18 ; Comparo los estados actual y previo por algun cambio
 	BREQ Loop ; Si no han cambiado, mantengo el loop
 	CALL Antirrebote
-	IN R16, PIND
+	IN R16, PINB
 	CP R16, R18 ; Comparo los estados actual y previo por algun cambio
 	BREQ Loop ; Si no han cambiado, mantengo el loop
 	; Si cambiaron
 	MOV R18, R16 ; Modifico el estado actual y
 	
 	CALL Contadores ; Verifico ambos contadores
+	
+	CALL Combinacion ; Combina los contadores para ponerlos en PIND
+
+	CALL Sumas ; Verifico el boton de Suma
 
 	RJMP Loop ; Al terminar vuelve al loop
 
@@ -86,32 +90,52 @@ Delay:
 ;-----------------------------------------------
 
 Contadores:
-; CONTADOR B R19
-AumentarB:
-	SBRC R16, PD0 ; Determino si el boton de aumentar esta presionado 
-	RJMP DecrementarB ; De no estar presionado, verifico el otro botón
+; CONTADOR 1 R19
+Aumentar1:
+	SBRC R16, PB0 ; Determino si el boton de aumentar esta presionado 
+	RJMP Decrementar1 ; De no estar presionado, verifico el otro botón
 	INC R19 ; De estar presionado, aumento contador R19
-	OUT PINB, R19 ; Y lo transporto a PINB
 
-DecrementarB:
-	SBRC R16, PD1 ; Determino si el boton de decrementar esta presionado 
-	RJMP AumentarC ; De no estar presionado, vuelvo al CALL
+Decrementar1:
+	SBRC R16, PB1 ; Determino si el boton de decrementar esta presionado 
+	RJMP Aumentar2 ; De no estar presionado, vuelvo al CALL
 	DEC R19 ; De estar presionado, disminuyo contador R19
-	OUT PINB, R19 ; Y lo transporto a PINB
 
-; CONTADOR C R20
-AumentarC:
-	SBRC R16, PD2 ; Determino si el boton de aumentar esta presionado 
-	RJMP DecrementarC ; De no estar presionado, verifico el otro botón
+; CONTADOR 2 R20
+Aumentar2:
+	SBRC R16, PB2 ; Determino si el boton de aumentar esta presionado 
+	RJMP Decrementar2 ; De no estar presionado, verifico el otro botón
 	INC R20 ; De estar presionado, aumento contador R20
-	OUT PINC, R20 ; Y lo transporto a PINC
 
-DecrementarC:
-	SBRC R16, PD3 ; Determino si el boton de decrementar esta presionado 
+Decrementar2:
+	SBRC R16, PB3 ; Determino si el boton de decrementar esta presionado 
 	RET ; De no estar presionado, vuelvo al CALL
 	DEC R20 ; De estar presionado, disminuyo contador R20
-	OUT PINC, R20 ; Y lo transporto a PINC
 
+	RET ; Al terminar, vuelvo al CALL
+
+;-----------------------------------------------
+
+Combinacion:
+	ANDI R19, 0X0F ; Modificar ambos contadores a solo 4 bits
+	ANDI R20, 0X0F
+	SWAP R20 ; Cambiar de lugar Nibbles de R20
+	MOV R21, R19 ; Copiar R19 a R21
+	OR R21, R20 ; Combinar el R19 con R20 en R21
+	SWAP R20 ; Regreso R20 a su forma original
+	OUT PORTD, R21 ; Despliego R21 en todo el PIND
+	RET ; Regreso al CALL
+
+;-----------------------------------------------
+
+
+Sumas:
+	SBRC R16, PB4; Determino si el boton de suma esta presionado
+	RET ; De no estar presionado, vuelvo al CALL
+	MOV R22, R19 ; De estar presionado copio R19 a R22
+	ADD R22, R20 ; Realizo la suma de R19 y R20 en R22
+	ANDI R22, 0X1F; Verifico que sean solo 5 bits (4 mas carry)
+	OUT PORTC, R22; Despliego el resultado en PORTC
 	RET ; Al terminar, vuelvo al CALL
 
 ;-----------------------------------------------
